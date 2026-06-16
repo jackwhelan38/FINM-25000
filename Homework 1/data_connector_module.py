fd1f905e3ff5e4b67e4b3547d71ccda2794df79f
Jack Whelan <jackwhelan@uchicago.edu>
2026-06-16 17:30:28 -0500

    more organization

---
+++ Homework 1/data_connector_module.py
@@ -0,0 +1,5 @@
+#Data Connector Module
+# - Loads API keys from environment variables
+# - Connects to Alpaca’s Market Data API
+# - Downloads historical data for a symbol you choose
+# - Streams real‑time quotes (bid/ask)
---
+++ Homework 1/historical_data_viewer.py
@@ -0,0 +1,4 @@
+#Historical Data Viewer
+# - Download at least 30 days of 1-minute or 5-minute bars
+# - Display them in a chart (matplotlib or plotly)
+# - Show OHLCV data (open, high, low, close, volume) clearly
