ed93a72c8e5e649b43c3dd2e4cd44c3307902c5e
Jack Whelan <jackwhelan@uchicago.edu>
2026-06-16 20:17:14 -0500

    added files as python

--- Homework 1/data_connector_module.py
+++ Homework 1/data_connector_module.py
@@ -3,3 +3,6 @@
 # - Connects to Alpaca’s Market Data API
 # - Downloads historical data for a symbol you choose
 # - Streams real‑time quotes (bid/ask)
+
+from alpaca.data.historical import StockHistoricalDataClient
+
